c2d3a41a042ff55f1d37ea7ed29af02c339530ec
Aditya17-4 <58966618+Aditya17-4@users.noreply.github.com>
2021-05-07 21:56:46 +0530

    final commit

--- webcam.py
+++ webcam.py
@@ -1,5 +1,4 @@
 import cv2
-import winsound
 
 cam = cv2.VideoCapture(0)
 while cam.isOpened():
@@ -11,14 +10,13 @@ while cam.isOpened():
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     dialated = cv2.dilate(thresh, None, iterations=3)
     contors, _ = cv2.findContours(dialated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.drawContours(frame1, contors, -1, (0, 255, 0), 2)
+
     for i in contors:
         if cv2.contourArea(i) < 10000:
             continue
         else:
             x, y, w, h = cv2.boundingRect(i)
             cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            # winsound.Beep(500, 200)
     if cv2.waitKey(10) == ord('q'):
         break
     cv2.imshow('My cam', frame1)
